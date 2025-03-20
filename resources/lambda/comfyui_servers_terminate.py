60169d03cfdce772eaf89416939ba9792ade37fc
vgames-global <65343048+vgames-global@users.noreply.github.com>
2025-03-20 13:40:35 +0800

    Add the server delete feature

---
+++ resources/lambda/comfyui_servers_terminate.py
@@ -0,0 +1,39 @@
+import boto3
+import json
+import os
+from comfyui_servers_dbutils import update_status, query_comfyui_servers_by_username
+
+comfyui_servers_table = os.environ.get('USER_COMFYUI_SERVERS_TABLE')
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table(comfyui_servers_table)
+ec2_client = boto3.client('ec2')
+
+def lambda_handler(event, context):
+
+    print(event)
+    body = json.loads(event['body'])
+    username = body.get('username','No body')
+    group_name = body.get('group_name','No Group')
+    result = query_comfyui_servers_by_username(username)
+    
+    if result:
+        for item in result:
+            stop_instance(item['instance_id'])
+            return {
+                "statusCode": 200,
+                "body": json.dumps({"instance_id": item['instance_id'], "code": 200})
+            }
+    else:
+        return {
+            "statusCode": 400,
+            "body": json.dumps({"message": f"No instance exists with username: {username}", "code": 400})
+        }
+
+def stop_instance(instance_id):
+    try:
+        response = ec2_client.terminate_instances(InstanceIds=[instance_id])
+        print(f'Successfully stopped instance: {instance_id}')
+        return response
+    except Exception as e:
+        print(f'Error terminate instance: {e}')
+        raise e
